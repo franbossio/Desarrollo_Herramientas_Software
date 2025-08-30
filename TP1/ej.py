bffc8cea17e2276f879f8724374d5386124e182d
unknown <franciscobossio10@gmail.com>
2025-08-30 19:25:09 -0300

    first commit

---
+++ TP1/ej.py
@@ -0,0 +1,2 @@
+datos = [1,5,3,2,20,52]
+print(max(datos))
--- TP1/trabajo_practico_1.py
+++ TP1/trabajo_practico_1.py
@@ -1,11 +1,11 @@
-def buscar_fecha(mayor_amp,menor_amp):
-    lista=[]
+def buscar_fecha(mayor_amp,menor_amp): #funcion para buscar la fecha especifica de medicion de una estacion
+    lista=[] 
     lista2=[]
     lista3=[]
     with open('registro_temperatura365d_smn.txt') as arc:
         lineas = arc.readlines()[3:]
         for linea in lineas:
-            lista.append(linea.strip())
+            lista.append(linea.strip()) #guardo el archivo en cada linea
        
         for listas in lista:
             if mayor_amp[0] in listas:
@@ -16,6 +16,7 @@ def buscar_fecha(mayor_amp,menor_amp):
         fecha2,resto2= lista3[menor_amp[2]].split(maxsplit=1)
     
     return fecha,fecha2
+
 datos = {}
 with open('registro_temperatura365d_smn.txt') as archivo:
     lineas = archivo.readlines()[3:]
@@ -23,23 +24,23 @@ with open('registro_temperatura365d_smn.txt') as archivo:
         partes = linea.strip().split(maxsplit=3)
         if len(partes)==4:
             fecha,temp1,temp2,nombre=partes
-            if '.' in temp1 and '.' not in temp2:
-                nombre = temp2 + ' ' + nombre
+            if '.' in temp1 and '.' not in temp2: #[17082025, 3.6, ESCUELA, DE AVIACION MILITAR AERO ]
+                nombre = temp2 + ' ' + nombre #escuela de aviacion miliatar aero
                 tmax,tmin=None,float(temp1)
             elif '.' not in temp1 and '.' not in temp2:
                 nombre = temp1 + temp2 + ' ' + nombre
                 tmax,tmin = None, None
             else: 
                 tmax,tmin = float(temp1),float(temp2)
-        elif len(partes)==3:
+        elif len(partes)==3: #[16082025,CHILECITO,AERO]
             fecha,temp_value,nombre=partes
             if '.' in temp_value:
                 tmax, tmin= None,float(temp_value)
             elif '.' not in temp_value:
-                nombre=temp_value + ' ' +nombre
+                nombre=temp_value + ' ' +nombre #chilecito aero
                 tmax,tmin=None,None
-        else:
-            fecha,nombre=partes
+        else:#len(partes)<=2 pero siempre es ==2 porque no puede ser ==1
+            fecha,nombre=partes #[17082025,JACHAL]
             tmax,tmin=None,None
         if nombre not in datos:
             datos[nombre]={'tmax':[],'tmin':[]}
@@ -55,15 +56,15 @@ with open('reporte.txt','w',encoding='UTF-8') as reporte:
         try:
             temperatura_maxima=max(x for x in valor['tmax'] if x is not None)
         except ValueError:
-            temperatura_maxima='No hay registros'
+            temperatura_maxima=' No hay registros'
         try:
             temperatura_minima=min(x for x in valor['tmin'] if x is not None)
         except ValueError:
-            temperatura_maxima='No hay registros'
+            temperatura_maxima=' No hay registros'
         
         reporte.write(f'\n{clave}: TMAX:{temperatura_maxima}, TMIN:{temperatura_minima}')
         
-    reporte.write('\n------------MAYOR Y MENOS AMPLITUD TERMICA EN UN DIA------------\n')
+    reporte.write('\n------------MAYOR Y MENOR AMPLITUD TERMICA EN UN DIA------------\n')
     
     mayor_amp = ("", -1, None)  # (est,amp,dia)
     menor_amp = ("", 9999, None)
@@ -82,8 +83,8 @@ with open('reporte.txt','w',encoding='UTF-8') as reporte:
                 menor_amp = (clave, amp, dia)
    
     fecha,fecha2=buscar_fecha(mayor_amp,menor_amp)
-    reporte.write(f"Mayor amplitud: {mayor_amp[0]} con {mayor_amp[1]}°C el día {fecha[:2]} del {fecha[2:4]} del año {fecha[4:]}\n")
-    reporte.write(f"Menor amplitud: {menor_amp[0]} con {menor_amp[1]}°C el día {fecha2[:2]} del {fecha2[2:4]} del año {fecha2[4:]}\n")
+    reporte.write(f"Mayor amplitud: {mayor_amp[0]} con {mayor_amp[1]}°C el día {fecha[:2]}/{fecha[2:4]}/{fecha[4:]}\n")
+    reporte.write(f"Menor amplitud: {menor_amp[0]} con {menor_amp[1]}°C el día {fecha2[:2]}/{fecha2[2:4]}/{fecha2[4:]}\n")
     
     reporte.write('\n------------DIFERENCIA MAXIMA Y MINIMA ENTRE ESTACCIONES EN UN MISMO DIA------------\n')
        
